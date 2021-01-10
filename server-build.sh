f5a997d777cb09ae8cf0e7f3f5ac24c56b03c6e4
Christoph Müller <mueller@olyro.de>
2021-01-10 18:59:33 +0100

    absolute deno path

--- server-build.sh
+++ server-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd blog
-deno run --unstable --allow-read --allow-write  blog.ts
+/home/runner/.deno/bin/deno run --unstable --allow-read --allow-write  blog.ts
 cd ..
 fdfind -e md -x pandoc -s {} --template template.html -c /mvp.css --metadata=github:"https://github.com/raytracer/website/edit/main/{}" -o {.}.html
