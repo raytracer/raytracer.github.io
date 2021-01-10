780e92853d557c824ebaa3c145e08e59331fbd91
Christoph Müller <mueller@olyro.de>
2021-01-10 18:43:47 +0100

    removed generate.ts

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 cd blog
 deno run --unstable --allow-read --allow-write  blog.ts
 cd ..
-fd -e md -x pandoc -s {} --template template.html -c /mvp.css -o {.}.html
+fd -e md -x pandoc -s {} --template template.html -c /mvp.css --metadata=github:"https://github.com/raytracer/website/edit/main/{}" -o {.}.html
